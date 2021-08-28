ce7ea4d2d18cc0aa9ad1912c136271ab8f54a1f8
Lunfir <lunfirok@gmail.com>
2021-08-28 13:23:47 +0300

    Add class Cat

---
+++ oop/cat.cpp
@@ -0,0 +1,11 @@
+#include "cat.h"
+
+void Cat::eat()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+void Cat::walk()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
---
+++ oop/cat.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+class Cat
+{
+public:
+    Cat() = default;
+    ~Cat() = default;
+
+    void eat();
+    void walk();
+};
